3cef4adb60456904f5e692ee22f4a96b153e6f84
Ray S. Lee <sleepred@gmail.com>
2023-02-21 10:51:03 +0900

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+export default function Home() {
+  return (
+    <>
+
+    <h1>
+      <Link herf="/">WEB</Link>
+    </h1>
+    <ol>
+      <li><Link href="/read/1">html</Link></li>
+      <li><Link href="/read/2">css</Link></li>      
+    </ol>
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB!!
+    </article>
+    <ul>
+      <li><Link href="/create">Create</Link></li>
+      <li><Link href="/update">Update</Link></li>
+      <li><Link href="/delete">Delete</Link></li>
+    </ul>
+    </>
+  )
+}
